f7f58c5827031444cf10906f13f1c84ee025c1ba
Shadow98057 <conner.bennett2000@gmail.com>
2013-04-05 16:45:13 -0700

    Update TotalEconomyVault.java

--- Total Economy Vault/src/com/gmail/erigitic/TotalEconomyVault.java
+++ Total Economy Vault/src/com/gmail/erigitic/TotalEconomyVault.java
@@ -2,7 +2,7 @@ package com.gmail.erigitic;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TotalEconomyVault extends JavaPlugin {
+public class TotalEconomyVault extends JavaPlugin 
 
 	public void onEnable() {
 		
